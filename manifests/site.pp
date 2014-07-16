e6cb08e5e66f9ad24bcac163202dd6a226155a95
Chris Cowley <chris@chriscowley.me.uk>
2014-07-16 22:45:10 +0200

    removed sensu

--- manifests/site.pp
+++ manifests/site.pp
@@ -6,16 +6,4 @@ node default inherits basenode {
 }
 
 node 'monitor.chriscowley.lan' {
-  class { 'sensu':
-    rabbitmq_password => 'secret',
-    server            => true,
-    dashboard         => true,
-    api               => true,
-    plugins           => [
-      'puppet:///data/sensu/plugins/ntp.rb',
-    ]
-  }
-  sensu::handler { 'default':
-    command => 'mail -s \'sensu alert\' chris@localhost'
-  }
 }
