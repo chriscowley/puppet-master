f4331443aed788e9a15edd65fed4801449d9bccc
Chris Cowley <chris@chriscowley.me.uk>
2014-07-16 22:39:58 +0200

    added Sensu basic server config

--- manifests/site.pp
+++ manifests/site.pp
@@ -4,3 +4,18 @@ node basenode {
 
 node default inherits basenode {
 }
+
+node 'monitor.chriscowley.lan' {
+  class { 'sensu':
+    rabbitmq_password => 'secret',
+    server            => true,
+    dashboard         => true,
+    api               => true,
+    plugins           => [
+      'puppet:///data/sensu/plugins/ntp.rb',
+    ]
+  }
+  sensu::handler { 'default':
+    command => 'mail -s \'sensu alert\' chris@localhost'
+  }
+}
