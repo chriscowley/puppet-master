a4989e3d2abc8dd8f9278aa0cc8c2340a497c229
Chris Cowley <chris@chriscowley.me.uk>
2014-10-29 21:28:33 +0100

    Allow 1GB file uploads in Owncloud

--- manifests/site.pp
+++ manifests/site.pp
@@ -8,6 +8,9 @@ node default inherits basenode {
 node 'ext.chriscowley.lan' inherits basenode {
   php::ini { '/etc/php.ini':
     memory_limit   => '256M',
+    upload_max_filesize => '1G',
+    post_max_size       => '1G',
+    output_buffering    => '0',
   }
 
   include php::cli
@@ -16,5 +19,6 @@ node 'ext.chriscowley.lan' inherits basenode {
   php::fpm::conf { 'www':
     listen  => '127.0.0.1:9000',
     user    => 'nginx',
-  }
+  {
 }
+
